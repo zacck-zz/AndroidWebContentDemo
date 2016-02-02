7cc6f5d9a12a5f9363cb244786f5d3050cffd5c8
zacck <coderv63@gmail.com>
2016-02-02 06:55:32 +0200

    got the Basic AsyncTask working and pulling content now to consume

--- app/src/main/java/com/zacck/androidwebcontentdemo/MainActivity.java
+++ app/src/main/java/com/zacck/androidwebcontentdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.zacck.androidwebcontentdemo;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //call the thread
+        DownloadTask mTask = new DownloadTask();
+        try
+        {
+            String mDone = mTask.execute("http://launchdesign.co.za").get();
+            Log.i(getPackageName()+" Done", mDone);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String>
+    {
+
+        @Override
+        protected String doInBackground(String... params) {
+            Log.i(getPackageName()+" Url", params[0]);
+            return "Done";
+        }
     }
 }
