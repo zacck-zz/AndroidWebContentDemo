32a66c1492db2f54582bff1c14b44274adf23917
zacck <coderv63@gmail.com>
2016-02-02 07:12:57 +0200

    Cleaned up the Asynctask to be able to use the result

--- app/src/main/java/com/zacck/androidwebcontentdemo/MainActivity.java
+++ app/src/main/java/com/zacck/androidwebcontentdemo/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         try
         {
             String mDone = mTask.execute("http://launchdesign.co.za").get();
-            Log.i(getPackageName()+" Done", mDone);
+            Log.i(getPackageName()+" Url Contents", mDone);
 
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -33,9 +37,38 @@ public class MainActivity extends AppCompatActivity {
     {
 
         @Override
-        protected String doInBackground(String... params) {
-            Log.i(getPackageName()+" Url", params[0]);
-            return "Done";
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL mUrl;
+            HttpURLConnection mUrlConnection = null;
+
+            //try
+            try
+            {
+                mUrl = new URL(urls[0]);
+                //opening the connection
+                mUrlConnection = (HttpURLConnection)mUrl.openConnection();
+                //load the data from the connection
+                InputStream  in = mUrlConnection.getInputStream();
+                //Read the stream
+                InputStreamReader mReader = new InputStreamReader(in);
+                int data = mReader.read();
+                //read the data to the end
+                while(data != -1)
+                {
+                    char currentChar = (char)data;
+                    result += currentChar;
+
+                    data = mReader.read();
+                }
+                return result;
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+                return "failed";
+            }
+
         }
     }
 }
